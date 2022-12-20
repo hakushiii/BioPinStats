54548dde1f73ef3591679d7e961db039fccdf16d
Jerome Alapad <jeromealapad13@gmail.com>
2022-12-20 14:43:38 +0800

    BUILD IMPLEMENTATIONS

--- app/src/main/java/com/example/biopinstats/authentication/RegisterFragment.kt
+++ app/src/main/java/com/example/biopinstats/authentication/RegisterFragment.kt
@@ -8,13 +8,6 @@ import android.view.ViewGroup
 import com.example.biopinstats.R
 
 class RegisterFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
