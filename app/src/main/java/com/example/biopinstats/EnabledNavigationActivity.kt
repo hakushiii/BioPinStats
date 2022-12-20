8ce1b1d90085a45507c7b822c47bdcfd082369ea
Jerome Alapad <jeromealapad13@gmail.com>
2022-12-20 13:57:02 +0800

    Created Navigation Activity and Created Fragments for Basic Application

---
+++ app/src/main/java/com/example/biopinstats/EnabledNavigationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.biopinstats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnabledNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enabled_navigation)
+    }
+}
